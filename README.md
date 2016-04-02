c9a5cec16752c6ee84b2f167333e5d6006709364
Pim De Witte <pim@whitespell.com>
2016-04-01 20:16:40 -0400

    Calling method renamed to Enqueue, documentation added

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Unity-MainThreadDispatcher
-A simple, thread-safe way of executing actions (Such as UI manipulations) on the Main Thread from Unity.
+# UnityMainThreadDispatcher
+
+A thread-safe way of dispatching IEnumerator functions to the main thread in unity. 
+### Version
+0.1 - Note that this has not yet been tested in production and has been developed for a game that is still in progress. 
+### Installation
+
+No dependencies needed other than Unity. This script was created in Unity 5.3, and has been tested in 5.3. 
+
+1. Download the UnityMainThreadDispatcher prefab and add it to your scene. 
+2. Download the UnityMainThreadDispatcher.cs script and add it to your prefab
+3. You can now dispatch objects to the main thread in Unity.
+###Installation
+```C#
+public IEnumerator ThisWillBeExecutedOnTheMainThread() {
+		Debug.Log ("This is executed from the main thread");
+		yield return null;
+	}
+	public void ExampleMainThreadCall() {
+		UnityMainThreadDispatcher.Instance().Enqueue(ThisWillBeExecutedOnTheMainThread()); 
+	}
+```
+
+### Development
+
+Want to contribute? Great! If you find a bug or want to make improvements, simply fork the repo and make a pull request with your changes on your own fork.
+
+### Author
+@PimDeWitte
+
+
+
+
+
+
+ 
--- UnityMainThreadDispatcher.cs
+++ UnityMainThreadDispatcher.cs
@@ -1,4 +1,20 @@
-﻿using UnityEngine;
+﻿/*
+Copyright 2015 Get Wrecked B.V. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
@@ -19,10 +35,10 @@ public class UnityMainThreadDispatcher : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Locks the queue and adds the action to the queue
+	/// Locks the queue and adds the IEnumerator to the queue
 	/// </summary>
 	/// <param name="action">IEnumerator function that will be executed from the main thread.</param>
-	public void AddAction(IEnumerator action) {
+	public void Enqueue(IEnumerator action) {
 		lock (_executionQueue) {
 			_executionQueue.Enqueue (() => {  
 				StartCoroutine (action); 
@@ -32,7 +48,6 @@ public class UnityMainThreadDispatcher : MonoBehaviour {
 
 
 	private static UnityMainThreadDispatcher _instance = null;
-	private bool _isInitialized = false;
 
 	public static bool Exists() {
 		return _instance != null;
